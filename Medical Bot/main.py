2325f0822f346ebd06fc9c41e360ac53d1c82568
oharakj2-dev <ohara.kj.2@gmail.com>
2025-08-20 20:02:39 -0500

    Added persistance to the Medical Bot

--- Medical Bot/main.py
+++ Medical Bot/main.py
@@ -1,5 +1,8 @@
 import os
 
+# Set file_path to point to 'patients.txt' 
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "patients.txt")
+
 welcome_prompt = "Welcome Doctor! How can I assist you today?\n - To list all patients, press 1 \n - To run a new diagnosis, press 2\n - To quit, press q\n"
 name_prompt = "Please enter patients name:\n"
 appearance_prompt = "Please describe the patient's appearance:\n - 1: Normal\n - 2: Irritable or lethargic\n"
@@ -21,9 +24,11 @@ patients_and_diagnosis = [
 def get_patient_data():
     #for patient in patients_and_diagnosis:
     #    print(patient)
-    patients = open("patients.txt", "r+")
-    print(patients.read())
-    patients.close()    
+    if not os.path.exists(file_path):
+        print("No patient data found.\n")
+        return
+    with open(file_path, "r") as patients:
+        print(patients.read())
 
 # Function to save a new diagnosis to patients.txt
 def save_new_diagnosis(name, diagnosis):
@@ -32,12 +37,12 @@ def save_new_diagnosis(name, diagnosis):
         return
     final_diagnosis = name + ", " + diagnosis
     #patients_and_diagnosis.append(final_diagnosis)
-    if not os.path.exists("patients.txt"):
-        with open("patients.txt", "w") as f:
+    if not os.path.exists(file_path):
+        with open(file_path, "w") as f:
             f.write(final_diagnosis + "\n")
             f.close()
     else: 
-        with open("patients.txt", "a") as f:
+        with open(file_path, "a") as f:
             f.write(final_diagnosis + "\n")
             f.close()
     print(final_diagnosis + " has been added to the patient list.\n") 
